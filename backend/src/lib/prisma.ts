29e2a1021b5659cb09dcf3cd7a17baf57c290db1
Katsuya <haradakatsuya190511@gmail.com>
2026-09-18 16:33:12 +0700

    created prisma.ts

---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { PrismaClient } from '@/lib/generated/prisma/client.js';
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DB_HOST!,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USERNAME!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
+});
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+
+export default prisma;
