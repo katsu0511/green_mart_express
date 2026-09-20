3b32a189c0b7cd756f0f87ef59f88910ccbb5049
Katsuya <haradakatsuya190511@gmail.com>
2026-09-20 12:05:14 +0700

    created Footer component

---
+++ frontend/src/components/Organisms/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='footer-content'>&copy; Green Mart</div>
+    </footer>
+  );
+}
