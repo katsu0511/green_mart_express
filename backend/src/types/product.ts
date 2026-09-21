95efdd9a10db95a0ed395e1a29d7088e4d36fe7f
Katsuya <haradakatsuya190511@gmail.com>
2026-09-21 18:12:31 +0700

    created Product type

---
+++ backend/src/types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id: number
+  categoryId: number
+  name: string
+  description: string
+  price: number
+  imageUrl: string
+  stock: number
+  createdAt: Date
+  updatedAt: Date
+};
