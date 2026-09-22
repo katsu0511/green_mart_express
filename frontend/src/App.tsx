d2e9ac6e03fd0eba9c8032cba6124491ad198f84
Katsuya <haradakatsuya190511@gmail.com>
2026-09-22 20:16:15 +0700

    added ModalProviderWrapper outside of MainLayout to allow use context of Modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ModalProviderWrapper from '@/components/Templates/ModalProviderWrapper/ModalProviderWrapper';
 import MainLayout from '@/components/Templates/MainLayout/MainLayout';
 import ProductList from '@/pages/ProductList/ProductList';
 import ProductDetail from '@/pages/ProductDetail/ProductDetail';
@@ -6,12 +7,14 @@ import ProductDetail from '@/pages/ProductDetail/ProductDetail';
 export default function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path='/' element={<ProductList />} />
-          <Route path='/products/:id' element={<ProductDetail />} />
-        </Routes>
-      </MainLayout>
+      <ModalProviderWrapper>
+        <MainLayout>
+          <Routes>
+            <Route path='/' element={<ProductList />} />
+            <Route path='/products/:id' element={<ProductDetail />} />
+          </Routes>
+        </MainLayout>
+      </ModalProviderWrapper>
     </BrowserRouter>
   );
 }
