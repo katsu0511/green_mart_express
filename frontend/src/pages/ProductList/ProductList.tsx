1d2c069c962ac358d927f3a6b8d233fbb98ba70c
Katsuya <haradakatsuya190511@gmail.com>
2026-09-23 15:26:54 +0700

    updated error handling

--- frontend/src/pages/ProductList/ProductList.tsx
+++ frontend/src/pages/ProductList/ProductList.tsx
@@ -1,24 +1,31 @@
 import './ProductList.css';
 import { useState, useEffect } from 'react';
 import type { Product } from '@/types/product';
+import useModal from '@/lib/useModal';
 import { Link } from 'react-router-dom';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function ProductList() {
   const [products, setProducts] = useState<Product[]>([]);
-
-  const getProducts = async () => {
-    const res = await fetch('http://localhost:3001/api/products');
-
-    if (res.ok) {
-      const data = await res.json();
-      setProducts(data);
-    }
-  };
+  const { setErrorMessage, setDisplayErrorModal } = useModal();
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
+    const getProducts = async () => {
+      const res = await fetch(`${API_BASE_URL}/api/products`);
+
+      if (res.ok) {
+        const productList: Product[] = await res.json();
+        setProducts(productList);
+      } else {
+        const data = await res.json();
+        setErrorMessage(data.error);
+        setDisplayErrorModal(true);
+      }
+    };
+
     getProducts();
-  }, []);
+  }, [setErrorMessage, setDisplayErrorModal]);
 
   return (
     <div className='product-list'>
