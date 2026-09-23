29725c772ca679e8fb1194353ac7a9971ecaef47
Katsuya <haradakatsuya190511@gmail.com>
2026-09-23 15:27:28 +0700

    created ThemeProviderWrapper

---
+++ frontend/src/components/Templates/ThemeProviderWrapper/ThemeProviderWrapper.tsx
@@ -0,0 +1,21 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: { textTransform: 'none' },
+      },
+    },
+  },
+});
+
+export default function ThemeProviderWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
