2e4d2b0b84efc718df8041026f2e19d0a0ae3ab1
Katsuya <haradakatsuya190511@gmail.com>
2026-09-23 15:25:38 +0700

    added ErrorModal to MainLayout

--- frontend/src/components/Templates/MainLayout/MainLayout.tsx
+++ frontend/src/components/Templates/MainLayout/MainLayout.tsx
@@ -1,8 +1,12 @@
 import './MainLayout.css';
+import useModal from '@/lib/useModal';
 import Header from '@/components/Organisms/Header/Header';
 import Footer from '@/components/Organisms/Footer/Footer';
+import ErrorModal from '@/components/Organisms/ErrorModal/ErrorModal';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  const { displayErrorModal } = useModal();
+
   return (
     <>
       <Header />
@@ -10,6 +14,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
         <div className='main-content'>{children}</div>
       </main>
       <Footer />
+      { displayErrorModal && <ErrorModal /> }
     </>
   );
 }
