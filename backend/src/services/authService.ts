315df177d50821102a393d0b74b93c280e4a7526
Katsuya <haradakatsuya190511@gmail.com>
2026-09-25 15:56:34 +0700

    determined type of user

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,16 +1,17 @@
+import type { AppUser, AuthUser } from '@/types/user.js';
 import { findUserByEmail, createUser } from '@/repositories/userRepository.js';
 import bcrypt from 'bcrypt';
 import { generateToken } from '@/lib/jwt.js';
 
 export const signup = async (name: string, email: string, password: string) => {
-  const existingUser = await findUserByEmail(email);
+  const existingUser: AuthUser | null = await findUserByEmail(email);
 
   if (existingUser) {
     throw new Error('Email is already registered');
   }
 
   const passwordHash = await bcrypt.hash(password, 10);
-  const user = await createUser({ name, email, passwordHash });
-  const token = generateToken(user.id);
+  const user: AppUser = await createUser({ name, email, passwordHash });
+  const token: string = generateToken(user.id);
   return { user, token };
 };
