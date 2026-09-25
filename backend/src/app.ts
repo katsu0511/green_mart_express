cbea8149a7f44632cc6f5bda7abf11fb80f3c111
Katsuya <haradakatsuya190511@gmail.com>
2026-09-25 17:35:19 +0700

    modified a bit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ import errorMiddleware from '@/middlewares/errorMiddleware.js';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.ORIGIN_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
