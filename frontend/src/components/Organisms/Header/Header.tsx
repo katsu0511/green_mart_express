9062f5652e1f40088d4409a7aeac1908ec97f8ed
Katsuya <haradakatsuya190511@gmail.com>
2026-09-25 20:07:34 +0700

    added link to cart page

--- frontend/src/components/Organisms/Header/Header.tsx
+++ frontend/src/components/Organisms/Header/Header.tsx
@@ -6,6 +6,9 @@ export default function Header() {
     <header className='header'>
       <div className='header-content'>
         <Link to='/' className='header-link'>Green Mart</Link>
+        <Link to='/cart' className='header-image'>
+          <img src='/cart.png' />
+        </Link>
       </div>
     </header>
   );
