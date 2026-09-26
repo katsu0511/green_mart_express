e0504825ab3cf791bdbf3c148da6fd9efd8eb45a
Katsuya <haradakatsuya190511@gmail.com>
2026-09-26 15:59:25 +0700

    included Category into Product

--- backend/src/repositories/productRepository.ts
+++ backend/src/repositories/productRepository.ts
@@ -1,10 +1,12 @@
-import type { Product } from '@/types/product.js';
+import type { Product } from '@/lib/generated/prisma/client.js';
 import prisma from '@/lib/prisma.js';
+import type { ProductWithCategory } from '@/types/product.js';
 
 export const findProducts = (): Promise<Product[]> => prisma.product.findMany();
 
-export const findProductById = (id: number): Promise<Product | null> => {
+export const findProductById = (id: number): Promise<ProductWithCategory | null> => {
   return prisma.product.findUnique({
     where: { id },
+    include: { category: true }
   });
 };
